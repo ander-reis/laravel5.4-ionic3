3b9bca05604edc36df20fba7e9037de6c841c8e9
Anderson Git <ander-reis@hotmail.com>
2018-08-18 19:59:47 -0300

    alterando webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,6 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
     .browserSync('http://localhost:8000');
+
+
+//git bare funcionando
